c2db1fa96c0c73ec80ab37ed20c5e28d94c6ee7d
opilane <opilane@HP-CZC2349HTY>
2023-05-09 11:36:55 +0300

    f

--- Mang.cs
+++ Mang.cs
@@ -10,13 +10,14 @@ namespace uss
     {
         public static void Main(string[] args)
         {
-            Console.BackgroundColor = ConsoleColor.DarkGray;
-            Console.SetWindowSize(80, 25);
+            int speed=100;
+            Console.SetWindowSize(80, 26);
             Walls walls = new Walls(80, 25);
             walls.Draw();
 
             Point p = new Point(4,5 ,'*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
+            Console.ForegroundColor = ConsoleColor.Green;
             snake.Drow();
 
             Food newfood = new Food(80, 25, '*');
@@ -27,12 +28,13 @@ namespace uss
 
             while (true)
             {
-                //if (walls.IsHit(snake)||snake.IsHitTail())
-                //{
-                //    break;
-                //}
+                if (walls.IsHit(snake)||snake.IsHitTail())
+                {
+                    break;
+                }
                 if (snake.Eat(food))
                 {
+                    speed-=1;
                     food = newfood.CreateFood();
                     Console.ForegroundColor = ConsoleColor.Green;
                     food.Draw();
@@ -41,6 +43,7 @@ namespace uss
                 }
                 else
                 {
+                    Console.ForegroundColor = ConsoleColor.Green;
                     snake.Move();
                 }
                 if (Console.KeyAvailable)
@@ -48,7 +51,7 @@ namespace uss
                     ConsoleKeyInfo key = Console.ReadKey();
                     snake.Moving(key.Key);
                 }
-                Thread.Sleep(100);
+                Thread.Sleep(speed);
             }
         }
     }
